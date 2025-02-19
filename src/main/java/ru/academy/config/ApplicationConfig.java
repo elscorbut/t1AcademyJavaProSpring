6b46a0b514ec38634b3daa3e16d609bf9986c395
VButolin <vbutolin@vtb.ru>
2025-02-19 13:03:38 +0300

    task 4 done

---
+++ src/main/java/ru/academy/config/ApplicationConfig.java
@@ -0,0 +1,23 @@
+package ru.academy.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class ApplicationConfig {
+
+    @Bean
+    DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName("org.postgresql.Driver");
+        config.setJdbcUrl("jdbc:postgresql://localhost:5432/userdb");
+        config.setUsername("admin");
+        config.setPassword("admin");
+
+        return new HikariDataSource(config);
+    }
+}
